1d74294f9b1ff97f04af5c4b158477784481a066
Zlatimir Mihaylov <zlatimirmihailov@gmail.com>
2015-05-21 21:00:30 +0300

    Objects

---
+++ UsingObjects/UsingObjects/generate-people.js
@@ -0,0 +1,22 @@
+﻿function generatePeople() {
+    function Person(first, last, age) {
+        this.first = first;
+        this.last = last;
+        this.age = age;
+    }
+
+    var firstNames = ['Ivan', 'Petur', 'Boril', 'Kiril'];
+    var lastNames = ['Petrov', 'Georgiev', 'Dimitrov', 'Manchev'];
+
+    var people = [];
+
+    var count = Math.floor(Math.random() * 100);
+
+    for (var i = 0; i < count; i++) {
+        people.push(new Person(firstNames[Math.floor((Math.random() * 4))],
+                        lastNames[Math.floor((Math.random() * 4))],
+                        Math.floor((Math.random() * 10) + (Math.random() * 100))));
+    }
+
+    return people;
+}
